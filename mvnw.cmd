c646157c5ffe4be6b23ff3212eeaab165e7947f7
Dusk <svladlen327603@gmail.com>
2025-03-06 13:02:06 +0300

    delete modulepath

--- mvnw.cmd
+++ mvnw.cmd
@@ -35,7 +35,7 @@
 @FOR /F "usebackq tokens=1* delims==" %%A IN (`powershell -noprofile "& {$scriptDir='%~dp0'; $script='%__MVNW_ARG0_NAME__%'; icm -ScriptBlock ([Scriptblock]::Create((Get-Content -Raw '%~f0'))) -NoNewScope}"`) DO @(
   IF "%%A"=="MVN_CMD" (set __MVNW_CMD__=%%B) ELSE IF "%%B"=="" (echo %%A) ELSE (echo %%A=%%B)
 )
-@SET PSModulePath=%__MVNW_PSMODULEP_SAVE%
+
 @SET __MVNW_PSMODULEP_SAVE=
 @SET __MVNW_ARG0_NAME__=
 @SET MVNW_USERNAME=
